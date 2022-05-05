491ea5f08db73555bd3104e1dd1e255c90239ca3
ser-07 <ser23887@gmail.com>
2022-05-05 22:09:22 +0530

    Added button labels

--- FrontEnd/button-component/src/components/ButtonsContent.js
+++ FrontEnd/button-component/src/components/ButtonsContent.js
@@ -7,41 +7,86 @@ const ButtonsContent = () => {
       <section>
         <h4>Colors</h4>
         <div className="btn-container">
-          <Button color={"default"} />
-          <Button color={"primary"} btnname="Primary" />
-          <Button color={"secondary"} btnname="Secondary" />
-          <Button color={"danger"} btnname="Danger" />
+          <div className="single-btn">
+            <p>{"<Button >"}</p>
+            <Button color={"default"} />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button color='primary' />`}</p>
+            <Button color={"primary"} btnname="Primary" />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button color="secondary" />`}</p>
+            <Button color={"secondary"} btnname="Secondary" />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button color="danger" />`}</p>
+            <Button color={"danger"} btnname="Danger" />
+          </div>
         </div>
       </section>
       <section>
         <h4>Varients</h4>
         <div className="btn-container">
-          <Button varient={"outline"} btnname="Outline" />
-          <Button varient={"text"} btnname="Text" />
+          <div className="single-btn">
+            <p>{`<Button varient='outline' />`}</p>
+            <Button varient={"outline"} btnname="Outline" />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button varient='text' />`}</p>
+            <Button varient={"text"} btnname="Text" />
+          </div>
         </div>
       </section>
       <section>
         <h4>Icons</h4>
         <div className="btn-container">
-          <Button startIcon="cart" btnname="Buy Now" />
-          <Button endIcon="start" btnname="Play" />
-          <Button startIcon="attach" endIcon="start" btnname="Bookmark" />
+          <div className="single-btn">
+            <p>{`<Button StartIcon='cart' />`}</p>
+            <Button startIcon="cart" btnname="Buy Now" />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button endIcon='start' />`}</p>
+            <Button endIcon="start" btnname="Play" />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button StartIcon='attach' />`}</p>
+            <Button startIcon="attach" endIcon="start" btnname="Bookmark" />
+          </div>
         </div>
       </section>
       <section>
         <h4>Sizes</h4>
         <div className="btn-container">
-          <Button size="sm" btnname="small" />
-          <Button size="md" btnname={"Medium"} />
-          <Button size="lg" btnname={"Large"} />
+          <div className="single-btn">
+            <p>{`<Button size='sm' />`}</p>
+            <Button size="sm" btnname="small" />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button size='md' />`}</p>
+            <Button size="md" btnname={"Medium"} />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button size='lg' />`}</p>
+            <Button size="lg" btnname={"Large"} />
+          </div>
         </div>
       </section>
       <section>
         <h4>Disabled Varients</h4>
         <div className="btn-container">
-          <Button disabled btnname="Disabled" />
-          <Button varient="text" disabled btnname="Disabled" />
-          <Button disableShadow btnname="Disable Shadow" />
+          <div className="single-btn">
+            <p>{`<Button disabled />`}</p>
+            <Button disabled btnname="Disabled" />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button varient='text' disabled />`}</p>
+            <Button varient="text" disabled btnname="Disabled" />
+          </div>
+          <div className="single-btn">
+            <p>{`<Button disableShadow />`}</p>
+            <Button disableShadow btnname="Disable Shadow" />
+          </div>
         </div>
       </section>
     </div>
