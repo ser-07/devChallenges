13b494935c321d356e7629d19d33968176c19a8b
ser-07 <ser23887@gmail.com>
2022-05-09 21:24:48 +0530

    Added Responsiveness

--- FrontEnd/button-component/src/App.js
+++ FrontEnd/button-component/src/App.js
@@ -7,6 +7,12 @@ function App() {
       <div className="app_container">
         <h1>Button Component</h1>
         <ButtonsContent />
+        <div>
+          <p>Created by Sreejith [ser-07] - devChallenges.io</p>
+          <a href="https://github.com/ser-07">GitHub</a>
+          <span>||</span>
+          <a href="https://twitter.com/SER__07">Twitter</a>
+        </div>
       </div>
     </div>
   );
